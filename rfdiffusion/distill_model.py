ff5a7ece55410c62f72ca3eb09a1175abc50b48a
kmitchell-eit <kmitchell@eit.org>
2025-03-12 16:43:46 +0000

    fixed verbose logging

--- rfdiffusion/distill_model.py
+++ rfdiffusion/distill_model.py
@@ -23,7 +23,7 @@ class RFDiffusionDistiller:
     """
     
     def __init__(self, config_path=None, teacher_ckpt_path=None, student_ckpt_path=None, 
-                 generator_ckpt_path=None, device_map=None, hide_warnings=True):
+                 generator_ckpt_path=None, device_map=None, verbose=True):
         """
         Initialize the distiller with teacher, student, and generator models
         
@@ -38,10 +38,12 @@ class RFDiffusionDistiller:
         """
         # Configure logging
         self._log = logging.getLogger(__name__)
-        logging.basicConfig(level=logging.INFO)
 
-        if hide_warnings:
-            logging.getLogger().addFilter(lambda record: record.levelno != logging.WARNING)
+        if verbose:
+            logging.basicConfig(level=logging.INFO)
+        else:
+            logging.basicConfig(level=logging.ERROR)
+
         
         # Get available devices
         self.cuda_available = torch.cuda.is_available()
